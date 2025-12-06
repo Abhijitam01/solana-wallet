e6c7c835f203dfaffc7e197a9c4f731d0788bd05
Abhijitam01 <abhijitamdubey31@gmail.com>
2025-12-07 00:47:42 +0530

    boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+// Default styles that can be overridden by your app
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      hi their
-      hi their to 
+     <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={[]} autoConnect>
+            <WalletModalProvider>
+                    <WalletMultiButton />
+                      <WalletDisconnectButton />
+                         hi their
+                         hi their to 
+            </WalletModalProvider>
+        </WalletProvider>
+     </ConnectionProvider>
     </>
   )
 }
